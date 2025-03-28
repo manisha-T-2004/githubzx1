7bf81e05fae3a69d965220bcb29cf4cb99f65438
unknown <mmanisha04623@gmail.com>
2025-03-28 12:41:23 +0530

    design

--- src/pages/Complete/Complete.jsx
+++ src/pages/Complete/Complete.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Complete.css'
+import { useNavigate } from 'react-router-dom'
 
 
 
 function Complete() {
+  const [completeTaskList, setCompleteTaskList] = useState(JSON.parse(localStorage.getItem("completedTask")) ?? [])
+
+  const navigate = useNavigate()
   return (
-    <div>
-      <div class="maincontainer">
+    <div class="maincontainer">
 
-        <div class="main">
-          <div class="container">
-            <div class="menu">
-              <nav>
-                <a>My Tasks</a>
-                <a onClick={() => navigate("/complete")}>Completed Tasks</a>
-              </nav>
+      <div class="main">
+        <div class="container">
 
-            </div>
-            <div class="content">
-              fjghrg
+          <div class="menu">
+            <nav>
+              <a onClick={() => navigate("/")}>My Tasks</a>
+              <a onClick={() => navigate("/Complete")}>Completed Tasks</a>
+            </nav>
+          </div>
 
-            </div>
+          <div class="task">
+            {
+              completeTaskList.map((item, index) => {
+                return (
+                  <div class='input'>
+                    <p>{item.description}</p>
+                  </div>
+                )
+              })
+            }
           </div>
 
         </div>
-
-
       </div>
-
     </div>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import { useNavigate } from 'react-router-dom'
+import { BsTrash3Fill } from "react-icons/bs";
+import { TiTick } from "react-icons/ti";
+
 
 function Home() {
+  const [taskValue, setTaskValue] = useState("")
+  const [taskList, setTaskList] = useState(JSON.parse(localStorage.getItem("tasklist")) ?? [])
+  const [completedTask, setCompletedTask] = useState(JSON.parse(localStorage.getItem("completedTask")) ?? [])
+
+
+
+  function onAddTaskClicked() {
+    let newTask = {
+      description: taskValue,
+      isCompleted: false
+    }
+
+    let list = taskList
+    list.push(newTask)
+    setTaskList(list)
+    setTaskValue("")
+
+    localStorage.setItem("tasklist", JSON.stringify(list))
+
+  }
+
+  function onDeleteClick(index) {
+    let dupArray = taskList.filter((item, i) => i !== index)
+    setTaskList(dupArray)
+
+    localStorage.setItem("tasklist", JSON.stringify(dupArray))
+  }
+
+  function onCompleteClicked(index, item) {
+    const removeTaskList = [...taskList];
+    removeTaskList.splice(index, 1);
+    setTaskList(removeTaskList);
+
+    localStorage.setItem("tasklist", JSON.stringify(removeTaskList))
+
+    let complete = completedTask
+    complete.push(item)
+    setCompletedTask(complete)
+
+    localStorage.setItem("completedTask", JSON.stringify(complete))
+
+
+  }
+  
   const navigate = useNavigate()
   return (
     <div>
@@ -12,37 +59,47 @@ function Home() {
           <div class="container">
             <div class="menu">
               <nav>
-                <a>My Tasks</a>
-                <a onClick={() => navigate("/complete")}>Completed Tasks</a>
+                <a onClick={() => navigate("/")}>My Tasks</a>
+                <a onClick={() => navigate("/Complete")}>Completed Tasks</a>
               </nav>
-
             </div>
 
-            <div class="content">
-              <div class="taskbox">
-                <input class='inp' type="text" placeholder='Enter your Task'></input>
+            <div className="content">
 
-                <button class="addtask">Add Task</button>
+              <div class="taskbox">
+                <input class='inp' value={taskValue} type="text" placeholder='Enter your Task' onChange={(event) => {
+                  setTaskValue(event.target.value)
+                }} />
+
+                <button class="addtask" onClick={onAddTaskClicked}>Add Task</button>
               </div>
 
               <div class="task">
-                
+                {
+                  taskList.map((item, index) => {
+                    return (
+                      <div class='input'>
+                        <p>{item.description}</p>
+                        <div class="icons">
+                          <TiTick onClick={() => onCompleteClicked(index, item)} />
+                          <BsTrash3Fill onClick={() => onDeleteClick(index)} />
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+
               </div>
             </div>
 
-
-
-
-
           </div>
-
         </div>
-
-
       </div>
+
     </div>
 
   )
 }
 
 export default Home
+
